6119507627ce1be9cb67915ff437ab14f99603a3
Alexey <alexey@Alexeys-MacBook-Pro.local>
2020-11-29 17:38:26 -0800

    Reconnect

--- LiveX/livex.js
+++ LiveX/livex.js
@@ -1,15 +1,20 @@
 (function (url) {
-    var socket = new WebSocket(url);
-    socket.onmessage = function (event) {
-        console.log("livex-onmessage:", event);
-        if (event.data == "@") {
-            window.location.reload();
+    function connect() {
+        var socket = new WebSocket(url);
+        socket.onmessage = function (event) {
+            console.log("livex-onmessage:", event);
+            if (event.data == "@") {
+                window.location.reload();
+            }
+        }
+        socket.onerror = function (event) {
+            console.log("livex-onerror:", event);
+            socket.close();
+        }
+        socket.onclose = function (event) {
+            console.log("livex-onclose:", event);
+            setTimeout(connect, 1000);
         }
     }
-    socket.onerror = function (event) {
-        console.log("livex-onerror:", event);
-    }
-    socket.onclose = function (event) {
-        console.log("livex-onclose:", event);
-    }
+    connect();
 })
